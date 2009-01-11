6125eb70daa09c37859d96b65575a6277f30fe13
luisfmunoz <luisfmunoz@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2009-01-11 21:32:01 +0000

    2009-01-11  <unknown>
    	* Release: 1.8.1
    	- Added head_print method, documentation and fixed the Makefile.
    	
    	Added full documentation with a few examples.
    	New head_print method, which I hope helps replacing the confusing syntax of
    	NCD::Check::lines (or whatever).
    	Updated the Makefile to build this module, too.
    
    
    
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/perl-CAF@190 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

--- src/main/perl/FileEditor.pm
+++ src/main/perl/FileEditor.pm
@@ -12,6 +12,33 @@ use LC::File;
 
 our @ISA = qw (CAF::FileWriter);
 
+=pod
+
+=head1 NAME
+
+CAF::FileEditor - Class for securely making minor changes in CAF
+applications.
+
+=head1 DESCRIPTION
+
+This class should be used whenever a file is to be opened for
+modifying its existing contents. For instance, if you want to add a
+single line at the beginning or the end of the file.
+
+As usual, all operations may be logged by passing a C<log> argument to
+the class constructor.
+
+=head2 Public methods
+
+=over
+
+=item new
+
+Returns a new object it accepts the same arguments as the constructor
+for C<CAF::FileWriter>
+
+=cut
+
 sub new
 {
     my $class = shift;
@@ -20,7 +47,92 @@ sub new
     $self->IO::String::open ($txt);
 }
 
+=pod
+
+=item open
+
+Synonym for C<new()>
+
+=cut
+
 sub open
 {
     return new(@_);
 }
+
+=pod
+
+=item set_contents
+
+Sets the contents of the file to the given argument. Usually, it
+doesn't make sense to use this method directly. Just use a
+C<CAF::FileWriter> object instead.
+
+=cut
+
+sub set_contents
+{
+    
+    return IO::String::open (@_);
+}
+
+=pod
+
+=item head_print
+
+Appends a line to the very beginning of the file.
+
+=back
+
+=cut
+
+sub head_print
+{
+    my ($self, $head) = @_;
+    my $txt = $self->string_ref();
+    $self->set_contents ($head . $$txt);
+    return $self;
+}
+
+__END__
+
+=pod
+
+=head1 EXAMPLES
+
+=head2 Appending to the end of a file
+
+For instance, you may want to append a line to the end of a file, if
+it doesn't exist already:
+
+    my $fh = CAF::FileEditor->open ("/foo/bar",
+                                    log => $self);
+    if (${$fh->string_ref()} !~ m{hello, world}m) {
+        print $fh "hello, world\n";
+    }
+    $fh->close();
+
+=head2 Cancelling changes in case of error
+
+This is a subclass of C<CAF::FileWriter>, so just do as you did with
+it:
+
+    my $fh = CAF::FileEditor->open ("/foo/bar",
+                                    log => $self);
+    $fh->cancel() if $error;
+    $fh->close();
+
+=head2 Appending a line to the beginning of the file
+
+Trivial: use the C<head_print> method:
+
+    my $fh = CAF::FileEditor->open ("/foo/bar",
+                                    log => $self);
+    $fh->head_print ("This is a nice header for my file");
+
+=head1 SEE ALSO
+
+This is class inherits from L<CAF::FileWriter(3pm)>, and thus from
+L<IO::String(3pm)>.
+
+=cut
--- src/test/perl/test-caffileeditor.pl
+++ src/test/perl/test-caffileeditor.pl
@@ -8,11 +8,14 @@ use strict;
 use warnings;
 use testapp;
 use CAF::FileEditor;
-use Test::More tests => 6;
+use Test::More tests => 7;
 use constant FILENAME => '/my/path';
 use constant TEXT => <<EOF;
 En un lugar de La Mancha, de cuyo nombre no quiero acordarme
-no ha mucho tiempo vivía un hidalgo de los de adarga antigua...
+no há mucho tiempo que vivía un hidalgo de los de lanza en astillero...
+EOF
+use constant HEADTEXT => <<EOF;
+... adarga antigua, rocín flaco y galgo corredor.
 EOF
 
 our $text = TEXT;
@@ -31,5 +34,8 @@ $fh->close();
 
 is ($opts{contents}, TEXT, "Attempted to write the file with the correct contents");
 $fh = CAF::FileEditor->open (FILENAME);
+$fh->head_print (HEADTEXT);
+is (${$fh->string_ref()}, HEADTEXT . TEXT,
+    "head_print method working properly");
 isa_ok ($fh, "CAF::FileEditor", "Correct class after open method");
 isa_ok ($fh, "CAF::FileWriter", "Correct class inheritance after open method");
