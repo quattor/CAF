03ac4c2620f384c926fc1b0c415dceda62da58df
stdweird <stijn.deweirdt@ugent.be>
2014-07-18 19:46:43 +0200

    fix typo, use it internally

--- src/main/perl/FileEditor.pm
+++ src/main/perl/FileEditor.pm
@@ -129,7 +129,7 @@ Seek to the end of the file.
 
 =cut
 
-sub seek_begin
+sub seek_end
 {
     my ($self) = @_;
     $self->seek(IO_SEEK_END);
@@ -164,7 +164,7 @@ sub replace_lines
     my ($self, $re, $goodre, $newvalue) = @_;
 
     my @lns;
-    $self->seek(IO_SEEK_BEGIN);
+    $self->seek_begin();
 
     while (my $l = <$self>) {
         if ($l =~ $re && $l !~ $goodre) {
@@ -174,7 +174,7 @@ sub replace_lines
         }
     }
     $self->set_contents (join("", @lns));
-    $self->seek(IO_SEEK_END);
+    $self->seek_end();
 }
 
 =pod
@@ -281,7 +281,7 @@ sub add_or_replace_lines
     } else {
         $self->set_contents (join ("", @lns));
     }
-    $self->seek(IO_SEEK_END);
+    $self->seek_end();
 }
 
 
@@ -398,7 +398,7 @@ sub remove_lines
         *$self->{LOG}->debug (5, "remove_lines ($fname): re = '$re'\tgoodre = '$goodre'");
     }
     my @lns;
-    $self->seek(IO_SEEK_BEGIN);
+    $self->seek_begin();
 
     while (my $l = <$self>) {
         unless ($l =~ $re && $l !~ $goodre) {
@@ -406,7 +406,7 @@ sub remove_lines
         }
     }
     $self->set_contents(join("", @lns));
-    $self->seek(IO_SEEK_END);
+    $self->seek_end();
 }
 
 __END__
