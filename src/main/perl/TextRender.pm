5a63ed6d959f03c5def126f0dcc2dd65f69a3c60
stdweird <stijn.deweirdt@ugent.be>
2014-11-15 10:34:19 +0100

    TextRender: improve fail message when dealing with non-existing tplname

--- src/main/perl/TextRender.pm
+++ src/main/perl/TextRender.pm
@@ -222,9 +222,12 @@ sub sanitize_template
     $tplname = "$self->{relpath}/$tplname" if $self->{relpath};
 
     $self->debug(3, "We must ensure that all templates lie below $self->{includepath}");
-    $tplname = abs_path("$self->{includepath}/$tplname");
+    my $abs_tplname = "$self->{includepath}/$tplname";
+    $tplname = abs_path($abs_tplname);
     if (!$tplname || !-f $tplname) {
-        return $self->fail("Non-existing template name $tplname given");
+        # abs_path returns undef on non-existing path; use this to avoid uninitialized warning
+        $tplname = '<undef>' if ! defined($tplname);
+        return $self->fail("Non-existing template name $tplname given (abs_path of $abs_tplname)");
     }
 
     # untaint and sanitycheck
