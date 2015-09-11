6a53db18f877d839eb342a7fbf3eeb402fe5038c
stdweird <stijn.deweirdt@ugent.be>
2015-09-11 09:16:51 +0200

    Initial CAF::Download commit

---
+++ src/main/perl/Download.pm
@@ -0,0 +1,155 @@
+# ${license-info}
+# ${developer-info}
+# ${author-info}
+# ${build-info}
+
+package CAF::Download;
+
+use strict;
+use warnings;
+
+# For re-export only
+use CAF::Download::URL qw(set_url_defaults);
+use parent qw(CAF::Object Exporter CAF::Download::URL);
+
+use Readonly;
+use LC::Exception qw (SUCCESS);
+
+our @EXPORT_OK = qw(set_url_defaults);
+
+# TODO: dependencies on curl and kinit
+
+Readonly::Hash my %DOWNLOAD_METHODS => {
+    http => [qw(lwp curl)],
+    file => [qw(lwp)],
+};
+
+Readonly::Array my @DOWNLOAD_PROTOCOLS => sort keys %DOWNLOAD_METHODS;
+
+# TODO: can we mix and match x509/krb5 also for security like TLS?
+Readonly::Hash my %DOWNLOAD_AUTHENTICATION => {
+    krb5 => [qw(gssapi kinit)],
+    x509 => [qw(lwp)],
+};
+
+# Handle failures. Stores the error message and log it verbose and
+# returns undef. All failures should use 'return $self->fail("message");'.
+# No error logging should occur in this module.
+sub fail
+{
+    my ($self, @messages) = @_;
+    $self->{fail} = join('', @messages);
+    $self->verbose("FAIL: ", $self->{fail});
+    return;
+}
+
+# Disclaimer: inspired by 
+#    NCM::Component::download (15.8)
+#    EDG::WP4::CCM::Fetch (15.8)
+#    File::Fetch (0.48)
+
+=pod
+
+=head1 NAME
+
+CAF::Download - Class for downloading content from remote servers.
+
+=head1 SYNOPSIS
+
+    use CAF::Download;
+
+=head1 DESCRIPTION
+
+This class simplyfies the downloading of content located on remote servers.
+It handles things like authentication, decryption, creating the actual file, ...
+
+=head2 Methods
+
+=over
+
+=item C<_initialize>
+
+Initialize the process object. Arguments:
+
+=over
+
+=item destination
+
+The destination of the download, e.g. a filename.
+
+=item urls
+
+A array reference of urls. Urls will be tried in order, first successful
+one is used. Providing more than one url can thus be used for failover.
+
+See C<prepare_urls> method for a description off the C<urls>.
+
+=back 
+
+It takes some extra optional arguments:
+
+=over
+
+=item C<log>
+
+A C<CAF::Reporter> object to log to.
+
+=item C<setup>
+
+Boolean to run the setup (or not). Default/undef is to run setup.
+
+=item C<cleanup>
+
+Boolean to run the cleanup (or not). Default/undef is to run cleanup.
+
+=back
+
+=cut
+
+sub _initialize
+{
+    my ($self, $destination, $urls, %opts) = @_;
+
+    $self->{destination} = $self->prepare_destination($destination);
+    $self->{urls} = $self->parse_urls($urls);
+
+    %opts = () if !%opts;
+
+    $self->{log} = $opts{log} if $opts{log};
+
+    $self->{setup} = (! defined($opts{setup}) || $opts{setup}) ? 1 : 0;
+    $self->{cleanup} = (! defined($opts{cleanup}) || $opts{cleanup}) ? 1 : 0;
+    $self->debug(1, "setup $self->{setup} cleanup $self->{cleanup}");
+
+    return SUCCESS;
+}
+
+=pod 
+
+=item C<prepare_destination>
+
+C<prepare_destination> prepares the destination.
+
+Returns the prepared destination on success, undef in case of failure (and sets the C<fail> attribute).
+No errors are logged.
+
+=cut
+
+# TODO: e.g.mkdir, check write rights, tempdir, in case there's no intermediate file...
+
+sub prepare_destination
+{
+    my ($self, $destination) = @_;
+
+    my ($newdest);
+
+    return $newdest;
+}
+
+=pod
+
+=back
+
+=cut
+
+1;
---
+++ src/test/perl/download.t
@@ -0,0 +1,44 @@
+use strict;
+use warnings;
+use Test::More;
+use Test::Quattor;
+use CAF::Download qw(set_url_defaults);
+use Test::MockModule;
+use Cwd;
+
+=pod
+
+=head1 SYNOPSIS
+
+Test all methods for C<CAF::Download>
+
+=over
+
+=item _initialize
+
+=cut
+
+
+my $d = CAF::Download->new("/tmp/dest", ["http://localhost"]);
+isa_ok($d, 'CAF::Download', 'is a CAF::Download instance');
+is($d->{setup}, 1, "default setup is 1");
+is($d->{cleanup}, 1, "default cleanup is 1");
+
+$d = CAF::Download->new("/tmp/dest", ["http://localhost"], setup => 0, cleanup => 0);
+isa_ok($d, 'CAF::Download', 'is a CAF::Download instance');
+is($d->{setup}, 0, "setup disabled / set to 0");
+is($d->{cleanup}, 0, "cleanup disabled / set to 0");
+
+=item prepare_destination
+
+=cut
+
+# TODO? what do we support?
+
+=pod
+
+=back
+
+=cut
+
+done_testing();
